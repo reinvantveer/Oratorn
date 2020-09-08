8613398a8b7a8c664b700dd3639b414c25800c17
Rein van 't Veer <reinvantveer@gmail.com>
2020-09-08 20:55:04 +0200

    add Ship instances, use a "while running" semantics, spawn ships

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from random import randint
+from typing import List
 
 import pygame
+from numpy import random
+
+from Ship import Ship
 
 DISPLAY_EXTENTS = (1024, 768)
 FPS_CLOCK = pygame.time.Clock()
@@ -15,21 +18,28 @@ def main():
     background = pygame.image.load('assets/images/background.png')
     background = pygame.transform.scale(background, DISPLAY_EXTENTS)
 
-    ship_image = pygame.image.load('assets/images/Python_logo-large.png')
-    ship_image_x_size = ship_image.get_rect().size[0]
-    ship_x_pos = randint(0, DISPLAY_EXTENTS[0] - ship_image_x_size)
-    ship_y_pos = 0
-    speed = 1
+    ships: List[Ship] = []
 
-    while True:
+    running = True
+    while running:
         FPS_CLOCK.tick(10)
 
         # Set the background
         screen.blit(background, (0, 0))
 
-        # Set the ship image
-        ship_y_pos += speed
-        screen.blit(ship_image, (ship_x_pos, ship_y_pos))
+        ship_spawning = random.normal(scale=1)
+        spawn_chance = 1.8
+        if ship_spawning > spawn_chance:
+            ships.append(Ship(DISPLAY_EXTENTS))
+
+        for ship in ships:
+            # Set the ship image
+            ship.ship_y_pos += ship.speed
+            screen.blit(ship.ship_image, (ship.ship_x_pos, ship.ship_y_pos))
+
+            # If the spaceship touches the bottom, you lose
+            if ship.ship_y_pos > DISPLAY_EXTENTS[1] - ship.ship_y_size:
+                running = False
 
         # Event handling
         events = pygame.event.get()
@@ -40,10 +50,10 @@ def main():
                     quit()
 
                 if event.key == pygame.K_UP:
-                    speed += 1
+                    ships[0].speed += 1
 
                 if event.key == pygame.K_DOWN:
-                    speed -= 1
+                    ships[0].speed -= 1
 
             if event.type == pygame.QUIT:
                 pygame.quit()
