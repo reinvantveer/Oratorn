f26024e26ae2b8a9acf4be02d877e44081359864
reinvantveer <reinvantveer@gmail.com>
2020-09-09 16:48:14 +0200

    add normal ship image, use as default

--- Ship.py
+++ Ship.py
@@ -5,7 +5,7 @@ import pygame
 
 class Ship:
     def __init__(self, display_extents) -> None:
-        self.ship_image = pygame.image.load('assets/images/Python_logo-large.png')
+        self.ship_image = pygame.image.load('assets/images/normal_ship.png')
         self.ship_image_x_size, self.ship_y_size = self.ship_image.get_rect().size
         self.ship_x_pos = randint(0, display_extents[0] - self.ship_image_x_size)
         self.ship_y_pos = 0
